d46e8aaabd68c9c52aef78c3b497599e9dbba706
Lê Duy Hải <duyha0207@gmail.com>
2024-12-05 09:10:30 +0700

    update

--- src/main/java/vn/duyhai/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/duyhai/jobhunter/config/SecurityConfiguration.java
@@ -18,8 +18,6 @@ import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
-import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
-import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.nimbusds.jose.jwk.source.ImmutableSecret;
@@ -43,6 +41,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http, CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
         http
             .csrf(c -> c.disable())
+            .cors(Customizer.withDefaults())
             .authorizeHttpRequests(
                 authz -> authz
                 .requestMatchers("/","/login").permitAll()
@@ -52,10 +51,10 @@ public class SecurityConfiguration {
             .authenticationEntryPoint(customAuthenticationEntryPoint)
             )
             //default exception 
-            .exceptionHandling( 
-                exceptions -> exceptions 
-                    .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint()) //401 
-                    .accessDeniedHandler(new BearerTokenAccessDeniedHandler())) //403
+            // .exceptionHandling( 
+            //     exceptions -> exceptions 
+            //         .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint()) //401 
+            //         .accessDeniedHandler(new BearerTokenAccessDeniedHandler())) //403
             .formLogin(f -> f.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
